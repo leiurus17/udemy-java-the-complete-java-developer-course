71aedab30ad4a6bfc662ae03eb0aa046d6e8a696
Marlon <martianworm17@gmail.com>
2018-04-21 11:00:33 +0800

    What Are Variables?

--- src/Hello.java
+++ src/Hello.java
@@ -5,11 +5,14 @@ public class Hello {
 
         int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 12;
-        int myThirdNumber = 6;
+        int myThirdNumber = myFirstNumber * 2;
 
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 
+        int difference = 1000 - myTotal;
+
         System.out.println(myFirstNumber);
         System.out.println(myTotal);
+        System.out.println(difference);
     }
 }
