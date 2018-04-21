48d70d5bfb8ebee11019f79e5fdc71b220ae7513
Marlon <martianworm17@gmail.com>
2018-04-21 11:14:07 +0800

    What Are Variables?

---
+++ src/com/kamotelabs/tutorial001/Main.java
@@ -0,0 +1,13 @@
+package com.kamotelabs.tutorial001;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // integer primitive variables
+        int myMinValue = -2_147483_648;
+        int myMaxValue = 2_147_483_647; // you can insert underscores for readability
+
+        // byte primitive variables
+    }
+}
