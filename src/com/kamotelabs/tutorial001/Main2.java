7ba1e23a819f8b195a2d79fe30d9223e3da7415b
Marlon <martianworm17@gmail.com>
2018-04-22 10:04:56 +0800

    Getting To Know Primitive Data Types

--- src/com/kamotelabs/tutorial001/Main2.java
+++ src/com/kamotelabs/tutorial001/Main2.java
@@ -10,5 +10,7 @@ public class Main2 {
         long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
         System.out.println("longTotal = " + longTotal);
 
+        short shortTotal = (short) (1000 * 10 * (byteValue + shortValue + intValue));
+        System.out.println("shortTotal = " + shortTotal);
     }
 }
