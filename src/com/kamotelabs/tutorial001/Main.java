bc09ffc2581c129db1344edf08a043d63b148e1f
Marlon <martianworm17@gmail.com>
2018-04-22 10:02:23 +0800

    Getting To Know Primitive Data Types

--- src/com/kamotelabs/tutorial001/Main.java
+++ src/com/kamotelabs/tutorial001/Main.java
@@ -5,13 +5,23 @@ public class Main {
     public static void main(String[] args) {
 
         // integer primitive variables
-        int myMinValue = -2_147483_648;
+        // int has a width of 32
+        int myMinValue = -2_147_483_648;
         int myMaxValue = 2_147_483_647; // you can insert underscores for readability
 
         // byte primitive variables
+        // byte has a width of 8
         byte myByteValue = 127;
+        byte myNewByteValue = (byte) (myByteValue / 2);
+        System.out.println("myNewByteValue = " + myNewByteValue);
 
         // short primitive variable
         short myShortValue = 32767;
+
+        // long primitive variable
+        // long has width of 64
+        long myLongValue = 9_223_372_036_854_775_007L;
+
+
     }
 }
---
+++ src/com/kamotelabs/tutorial001/Main2.java
@@ -0,0 +1,14 @@
+package com.kamotelabs.tutorial001;
+
+public class Main2 {
+
+    public static void main (String[] args) {
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+        System.out.println("longTotal = " + longTotal);
+
+    }
+}
