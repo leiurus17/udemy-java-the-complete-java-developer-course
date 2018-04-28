0155bf4ee681f171c25006c2b65cb00eff85cb9a
Marlon <martianworm17@gmail.com>
2018-04-28 19:02:15 +0800

    Getting To Know Primitive Data Types

--- src/com/kamotelabs/tutorial001/Main3.java
+++ src/com/kamotelabs/tutorial001/Main3.java
@@ -3,9 +3,9 @@ package com.kamotelabs.tutorial001;
 public class Main3 {
 
     public static void main(String[] args) {
-        int myIntValue = 5 / 2;
-        float myFloatValue = 5f / 2f;
-        double myDoubleValue = 5d / 2d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
 
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("myFloatValue = " + myFloatValue);
