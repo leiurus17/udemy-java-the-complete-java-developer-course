9ccbaedc2e9c5cf7fa77f3211d9799bbbb3d5681
Marlon <martianworm17@gmail.com>
2018-04-29 20:49:49 +0800

    Getting To Know Primitive Data Types

--- src/com/kamotelabs/tutorial001/Main4.java
+++ src/com/kamotelabs/tutorial001/Main4.java
@@ -4,5 +4,11 @@ public class Main4 {
 
     public static void main(String[] args) {
         char myChar = 'D';
+        char unicode = '\u00A9';
+
+        System.out.println(myChar);
+        System.out.println(unicode);
+
+
     }
 }
