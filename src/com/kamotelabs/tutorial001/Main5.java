970a637215704ec8bd7c44c143f76a0ed514c0cf
Marlon <martianworm17@gmail.com>
2018-04-30 21:42:18 +0800

    Understanding Strings And Finishing Up Primitive Data Types

--- src/com/kamotelabs/tutorial001/Main5.java
+++ src/com/kamotelabs/tutorial001/Main5.java
@@ -18,5 +18,18 @@ public class Main5 {
         System.out.println("myString is equal to " + myString);
         myString = myString + " \u00A9 2015";
         System.out.println("myString is equal to " + myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println("The result is" + numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("lastString is equal to " + lastString);
+
+        double doubleNumber = 120.47;
+        lastString = lastString + doubleNumber;
+        System.out.println("lastString value: " + lastString);
     }
 }
