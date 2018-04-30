fbbee3361bcbdba1b21193fd87a6c88d42076c5b
Marlon <martianworm17@gmail.com>
2018-04-30 21:20:40 +0800

    Understanding Strings And Finishing Up Primitive Data Types

---
+++ src/com/kamotelabs/tutorial001/Main5.java
@@ -0,0 +1,22 @@
+package com.kamotelabs.tutorial001;
+
+public class Main5 {
+
+    public static void main(String[] args) {
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2015";
+        System.out.println("myString is equal to " + myString);
+    }
+}
