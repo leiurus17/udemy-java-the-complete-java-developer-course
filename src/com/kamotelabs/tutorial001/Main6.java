04242057429884493dd6dd66615b23890651821b
Marlon <martianworm17@gmail.com>
2018-05-01 11:25:28 +0800

    Operators In Java

--- src/com/kamotelabs/tutorial001/Main6.java
+++ src/com/kamotelabs/tutorial001/Main6.java
@@ -5,6 +5,21 @@ public class Main6 {
     public static void main(String[] args) {
         // Operators
         int result = 1 + 2;
-        System.out.println(result);
+        System.out.println("1 + 2 = " + result);
+
+        int previousResult = result;
+
+        result = result - 1;
+        System.out.println(previousResult + " - 1 = " + result);
+
+        previousResult = result;
+
+        result = result * 10;
+        System.out.println(previousResult + " * 10 = " + result);
+
+        previousResult = result;
+
+        result = result / 5;
+        System.out.println(previousResult + " / 5 = " + result);
     }
 }
