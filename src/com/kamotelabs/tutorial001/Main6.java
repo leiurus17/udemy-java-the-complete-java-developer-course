4fe671032b519e5ee81b5774d915e0aaf7af17f5
Marlon <martianworm17@gmail.com>
2018-05-01 11:19:57 +0800

    Operators In Java

---
+++ src/com/kamotelabs/tutorial001/Main6.java
@@ -0,0 +1,10 @@
+package com.kamotelabs.tutorial001;
+
+public class Main6 {
+
+    public static void main(String[] args) {
+        // Operators
+        int result = 1 + 2;
+        System.out.println(result);
+    }
+}
