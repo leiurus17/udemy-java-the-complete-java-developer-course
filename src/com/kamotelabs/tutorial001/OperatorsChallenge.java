8d8fd1e91f563d3f080b64b38e2dc773816486fd
Marlon <martianworm17@gmail.com>
2018-05-03 21:00:52 +0800

    More On Operators And Operator Precedence

--- src/com/kamotelabs/tutorial001/OperatorsChallenge.java
+++ src/com/kamotelabs/tutorial001/OperatorsChallenge.java
@@ -3,12 +3,14 @@ package com.kamotelabs.tutorial001;
 public class OperatorsChallenge {
 
     public static void main(String[] args) {
-        double firstDouble = 20L;
-        double secondDouble = 80L;
+        double firstDouble = 20D;
+        double secondDouble = 80D;
 
         double product = (firstDouble + secondDouble) * 25;
 
         double remainder = product % 40;
+
+        System.out.println(remainder);
 
         if (remainder >= 20) {
             System.out.println("Total was over the limit.");
