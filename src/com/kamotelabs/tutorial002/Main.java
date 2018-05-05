063d7a5fa8cb60663531e87d4144db592bbfdfee
Marlon <martianworm17@gmail.com>
2018-05-05 17:19:27 +0800

    Just add another line again

--- src/com/kamotelabs/tutorial002/Main.java
+++ src/com/kamotelabs/tutorial002/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Prepare for Section 5.");
+        System.out.println("This is just an additional line.");
     }
 }
