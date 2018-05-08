0b1b883e35d35df880ad9922a44a15d4bd6c8d7d
Marlon <martianworm17@gmail.com>
2018-05-08 20:14:13 +0800

    Statements, Whitespace and Indentation

---
+++ src/com/kamotelabs/tutorial002/Main2.java
@@ -0,0 +1,21 @@
+package com.kamotelabs.tutorial002;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        int myVariable = 50; // The whole line is a statement. myVariable = 50 is an expression.
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is a test");
+
+        System.out.println("This is " +
+            "another " +
+            "statement. " +
+            "But wait!");
+
+        int anotherVariable = 100; myVariable++; // This is valid but this is ugly!
+        
+
+    }
+}
