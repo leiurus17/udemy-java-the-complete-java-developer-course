30c3d9129c5497bd46a2ff62e1cbc11484b75682
Marlon <martianworm17@gmail.com>
2018-05-08 20:03:55 +0800

    Keywords And Expressions

--- src/com/kamotelabs/tutorial002/Main.java
+++ src/com/kamotelabs/tutorial002/Main.java
@@ -5,5 +5,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Prepare for Section 5.");
         System.out.println("This is just an additional line.");
+
+        int int2 = 5;
+
+        // false, true, null -- some of reserved words
+
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is an expression. The highScore == 50");
+        }
     }
 }
