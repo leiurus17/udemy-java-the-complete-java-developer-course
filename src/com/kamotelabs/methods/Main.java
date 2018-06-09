91ed747c7a61060f0414abf9435fc05d7aaf099d
Marlon <martianworm17@gmail.com>
2018-06-09 10:53:04 +0800

    Methods

---
+++ src/com/kamotelabs/methods/Main.java
@@ -0,0 +1,20 @@
+package com.kamotelabs.methods;
+
+public class Main {
+
+    public static void main(String[] args) {
+        calculateScore(true, 800, 5, 100);
+    }
+
+    public static void calculateScore(boolean gameOver,
+                                      int score,
+                                      int levelCompleted,
+                                      int bonus) {
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted + bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+}
