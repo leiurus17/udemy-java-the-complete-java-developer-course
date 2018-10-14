0907a557b4837832c92bd0de3922d828ee2b9316
Marlon Monzon <martianworm17@gmail.com>
2018-10-14 09:29:52 +0800

    Done with the Methods challenge

--- src/com/kamotelabs/methods/Methods2.java
+++ src/com/kamotelabs/methods/Methods2.java
@@ -20,14 +20,29 @@ public class Methods2 {
     //
 
     public static void main(String[] args) {
-
+        displayHighScorePosition("Marlon", 5083);
+        displayHighScorePosition("Sasuke", 999);
+        displayHighScorePosition("Naruto", 345);
     }
 
     public static void displayHighScorePosition(String playerName, int score) {
 
+        System.out.println(playerName
+                + " managed to get into position "
+                + calculateHighScorePosition(score)
+                + " on the high score table.");
+
     }
 
     public static int calculateHighScorePosition(int score) {
-        return 0;
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500 && score < 1000) {
+            return 2;
+        } else if (score >= 100 && score < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
